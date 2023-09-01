6623d518d9a6d0d89b024ba81da30cdb0a10e0c9
SachdevaSagar <143718050+SachdevaSagar@users.noreply.github.com>
2023-09-01 16:43:05 +0530

    Update RunAppiumServer.sh

--- scripts/RunAppiumServer.sh
+++ scripts/RunAppiumServer.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 set -ex
+
+# Install Appium and its drivers
 npm install -g appium
-# npm install appium doctor -g
-# appium driver install uiautomator2
+# npm install -g appium-doctor  # Optional: Install appium-doctor for additional checks
+# appium-doctor --ios           # Optional: Check the iOS setup with appium-doctor
 appium driver install xcuitest
 appium driver install --source=npm appium-flutter-driver
 
+# Print the installed Appium version for verification
 appium -v
-appium &>/dev/null &
+
+# Start Appium in the background
+appium &
+
+# Wait for Appium to fully start (you may need to adjust the sleep duration)
+sleep 10  # Adjust as needed
+
+# Optionally, you can add more commands to run your tests here
+
+# Terminate Appium (if needed) when tests are finished
+# pkill -f "appium"
