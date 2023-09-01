6d8c4859690de1a8b4cc860dbcddaa83c396e49f
SachdevaSagar <143718050+SachdevaSagar@users.noreply.github.com>
2023-09-01 15:13:44 +0530

    Update RunAppiumServer.sh

--- scripts/RunAppiumServer.sh
+++ scripts/RunAppiumServer.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 set -ex
-
-# Make sure Appium is installed
-if ! command -v appium &>/dev/null; then
-  npm install -g appium
-fi
-
-# Check if the xcuitest driver is installed
-if appium driver list --installed | grep -q "xcuitest"; then
-  # If it's installed, update it
-  appium driver update xcuitest
-else
-  # If it's not installed, install it
-  appium driver install xcuitest
-fi
-
-# Install the appium-flutter-driver
+npm install -g appium
+# npm install appium doctor -g
+# appium driver install uiautomator2
+appium driver install xcuitest
 appium driver install --source=npm appium-flutter-driver
 
-# Print the Appium version
 appium -v
-
-# Start the Appium server in the background
 appium &>/dev/null &
